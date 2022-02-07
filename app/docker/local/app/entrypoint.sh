734a7816b21e0ccae1c77cf145aa182306dd5316
marianobrc <marianomartinez84@gmail.com>
2022-02-07 10:57:49 -0300

    Update db host name in entrypoint script

--- app/docker/local/app/entrypoint.sh
+++ app/docker/local/app/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # When starting the django app container, we need to wait until the postgress DB is ready to receive connections
-# (docker-compose "depends_on: - quick-pay-db" is not enough to check that the quick-pay-db is ready)
+# docker-compose "depends_on: - db" checks the container started, but is not enough to check that the database is ready to take connections
 # This script also accepts a command to be executed after the DB is ready (i.e. migrate, runserver or a script..)
 function postgres_ready(){
 python << END
 import sys
 import psycopg2
 try:
-    conn = psycopg2.connect(dbname="$DB_NAME", user="$DB_USER", password="$DB_PASSWORD", host="quick-pay-db")
+    conn = psycopg2.connect(dbname="$DB_NAME", user="$DB_USER", password="$DB_PASSWORD", host="db")
 except psycopg2.OperationalError:
     sys.exit(-1)
 sys.exit(0)
