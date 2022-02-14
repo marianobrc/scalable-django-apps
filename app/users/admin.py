bc99f9ffe9e12d9c3e79822ba0767f3be5ecee1a
marianobrc <marianomartinez84@gmail.com>
2022-02-14 10:08:26 -0300

    Register the custom user model in the django admin

--- app/users/admin.py
+++ app/users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+admin.site.register(CustomUser, UserAdmin)
