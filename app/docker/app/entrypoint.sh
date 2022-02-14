47952afc40dfc68f98ecc13a272fd707f53d2270
marianobrc <marianomartinez84@gmail.com>
2022-02-14 11:52:34 -0300

    Make the entrypoint script more verbose

--- app/docker/app/entrypoint.sh
+++ app/docker/app/entrypoint.sh
@@ -7,8 +7,10 @@ python << END
 import sys
 import psycopg2
 try:
-    conn = psycopg2.connect(dbname="$DB_NAME", user="$DB_USER", password="$DB_PASSWORD", host="db")
-except psycopg2.OperationalError:
+    print("Trying to connect to database '$DB_NAME' with user '$DB_USER' on host '$DB_HOST'..")
+    conn = psycopg2.connect(dbname="$DB_NAME", user="$DB_USER", password="$DB_PASSWORD", host="$DB_HOST")
+except psycopg2.OperationalError as e:
+    print(e)
     sys.exit(-1)
 sys.exit(0)
 END
